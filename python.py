7bf06279e8d2cd26ec7a985d016bd228824f4399
Thomas Nixon <tomjnixon@googlemail.com>
2010-04-14 15:55:54 +0100

    Moved python execution into a subprocess that is automatically killed after 0.5 seconds. Bernard gave up trying to hack it, and therefore it is secure ;)
    (also, made whitespace a little prettier.)

--- python.py
+++ python.py
@@ -1,5 +1,6 @@
 import bot, re
 import __builtin__
+import multiprocessing, time
 
 evalFinder = re.compile("eval\((.+)\)[^\)]*")
 equalFinder = re.compile("(\w*)\s*=\s*(.*)")
@@ -21,6 +22,10 @@ default_locals = dict((k, v)
 		      if k in allowed_builtins)
 
 default_locals["__builtins__"] = None
+default_locals["sleep"] = time.sleep
+
+def truncate(msg):
+	return msg if len(msg) < 200 else msg[:200] + "..."
 
 requiredBeardBotVersion = 0.1
 class BeardBotModule(bot.BeardBotModule):
@@ -35,17 +40,37 @@ class BeardBotModule(bot.BeardBotModule):
 		try:
 			if eval_found:
 				source = eval_found.group(1)
-				result = self.eval(source)
+				result = self.eval_timed(source)
 				self.locals['_'] = result
-				self.bot.say(repr(result)[:200])
+				self.bot.say(truncate(repr(result)))
 			elif equal_found:
 				dest = equal_found.group(1)
 				source = equal_found.group(2)
-				self.locals[dest] = self.eval(source)
-				self.bot.say("%s = %s" % (dest, self.locals[dest]))
+				self.locals[dest] = self.eval_timed(source)
+				self.bot.say(truncate("%s = %s" % (dest, self.locals[dest])))
 		except Exception, e:
 			self.bot.say("I'm afraid I can't let you do that, Dave.")
+			print dir(e)
 
 	def eval(self, source):
 		return eval(source, self.locals, self.locals)
+
+	def eval_timed(self, source):
+		def target(self, source, conn, done):
+			conn.send(self.eval(source))
+			conn.close()
+			done.set()
+			
+
+		parent_conn, child_conn = multiprocessing.Pipe()
+		done = multiprocessing.Event()
+		p = multiprocessing.Process(target=target, args=(self, source, child_conn, done))
+		p.start()
+		done.wait(0.5)
+		p.terminate()
+		if done.is_set():
+			return parent_conn.recv()
+		else:
+			raise Exception()
+		
 	
