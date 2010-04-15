052098b0d12f2ffffcf9c6b5c42abc1a808ac5d0
Thomas Nixon <tomjnixon@googlemail.com>
2010-04-15 19:08:07 +0100

    Changed wtf module to use new re decorator.

--- wtf.py
+++ wtf.py
@@ -1,22 +1,19 @@
 import bot, re, string
 
-ask_re = re.compile("wtf is (\S*[^\?])\??")
-
 
 word_lists = ["/usr/share/misc/acronyms", "/usr/share/misc/acronyms.comp"]
 
 
 requiredBeardBotVersion = 0.1
 class BeardBotModule(bot.BeardBotModule):
-	def on_channel_message(self, source_name, source_host, message):
-		found = ask_re.search(message)
-		if found:
-			word = found.group(1)
-			description = self.translate(word)
-			if description:
-				self.bot.say(description)
-			else:
-				self.bot.say("Fuck knows!?")
+
+	@bot.on_channel_match("wtf is (\S*[^\?])\??")
+	def define(self, source_name, source_host, message, word):
+		description = self.translate(word)
+		if description:
+			self.bot.say(description)
+		else:
+			self.bot.say("Fuck knows!?")
 
 
 	def translate(self, word_to_find):
