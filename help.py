dd1f6d7ce73191db4b9cd94196d4ec5472614cbd
Thomas Nixon <tomjnixon@googlemail.com>
2010-04-16 03:04:40 +0100

    Added a simple help module.

---
+++ help.py
@@ -0,0 +1,20 @@
+from base_module import *
+import re
+import string
+
+requiredBeardBotVersion = 0.1
+class BeardBotModule(ModuleBase):
+	"""help [module_name]
+	"""
+	@on_addressed_match("help (?:me )?(?:on |with )?(\S+)", re.I)
+	def help(self, source_name, source_host, message, module_name):
+		if module_name in self.bot.modules:
+			module = self.bot.modules[module_name].__class__
+			if hasattr(module, "__doc__") and module.__doc__:
+				doc = module.__doc__
+				for line in filter(len, map(string.rstrip, doc.split("\n"))):
+					self.bot.say(line)
+			else:
+				self.bot.say("An undocumented module? Your guess is as good as mine!")
+		else:
+			self.bot.say("That's not a module! You need to see a psychiatrist, not an irc bot.")
