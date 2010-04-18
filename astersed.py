64d5b6a3479ddc23a719c3da2507bdb89beb3f4a
Jonathan Heathcote <mail@jhnet.co.uk>
2010-04-18 01:13:42 +0100

    Made astersed use decorators (sorry tom)

--- astersed.py
+++ astersed.py
@@ -1,27 +1,18 @@
 from base_module import *
-import re, difflib, shelve
-
-# Matches a sed-style regex
-regexAster = re.compile("\*([^\s]+)")
+import difflib, shelve
 
 requiredBeardBotVersion = 0.1
 class BeardBotModule(ModuleBase):
 	def __init__(self, *args, **kwargs):
 		ModuleBase.__init__(self, *args, **kwargs)
 		self.messages = shelve.open(self.bot.channel + "_astersed.db")
-		print __name__
 	
 	def on_channel_message(self, source_name, source_host, message):
-		regex = regexAster.match(message)
-		if regex:
-			change = regex.groups()[0]
-			self.do_substitution(change, source_name)
-		else:
-			self.messages[source_name] = message
+		self.messages[source_name] = message
 	
-	def do_substitution(self, change, user):
-		if user in self.messages:
-			last_message = self.messages[user].split()
+	@on_channel_match("\*([^\s]+)")
+	def on_aster_change(self, source_name, source_host, message, change):
+			last_message = self.messages[source_name].split()
 			likely_changes = difflib.get_close_matches(change, last_message, 5, 0.5)
 			
 			# Don't match an exact version of the word
